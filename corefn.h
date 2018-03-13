452ebcfd6213378a3a1c53d3deb78a3c79cb1ece
Weixiang Weng <wengweixiangchina@gmail.com>
2018-03-13 08:36:45 -0500

    Removed echo function

---
+++ corefn.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "codegen.h"
+#include "node.h"
+
+using namespace std;
+
+extern int yyparse();
+extern NProgram* programBlock;
+
+llvm::Function* createPrintfFunction(CodeGenContext& context) {
+    std::vector<llvm::Type*> printf_arg_types;
+    printf_arg_types.push_back(llvm::Type::getInt8PtrTy(MyContext)); //char*
+
+    llvm::FunctionType* printf_type =
+        llvm::FunctionType::get(llvm::Type::getInt32Ty(MyContext), printf_arg_types, true);
+
+    llvm::Function *func = llvm::Function::Create(printf_type, llvm::Function::ExternalLinkage,
+                llvm::Twine("printf"),context.module);
+    func->setCallingConv(llvm::CallingConv::C);
+    
+    return func;
+}
+
+void createCoreFunctions(CodeGenContext& context){
+    llvm::Function* printfFn = createPrintfFunction(context);
+}
